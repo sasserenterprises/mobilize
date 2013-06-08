fa35017bd38741f78fda8ccc1b2df2ae1d876ce0
Daniel Sasser <daniel@sasserenterprises.com>
2013-06-08 12:53:54 -0500

    blah

--- assets/www/index.js
+++ assets/www/index.js
@@ -4,4 +4,9 @@ http://www.hashemian.com/
 Revised to jQuery June 8, 2013 by Daniel T Sasser II
 http://www.s2-designs.com
 */
-$.mobile.allowCrossDomainPages = true;
+$( document ).bind( "mobileinit", function() {
+    // Make your jQuery Mobile framework configuration changes here!
+
+    $.mobile.allowCrossDomainPages = true;
+	$.support.cors = true;
+});
